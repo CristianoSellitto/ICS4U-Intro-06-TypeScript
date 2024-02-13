dcdf34c343001a6e5a1927276883e1bf41843c78
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2024-02-13 14:55:05 +0000

    Added Hello, World program

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* By:      Cristiano Sellitto
+* Version: 1.0
+* Since:   2024-02-13
+*/ 
+
+console.log("Hello, World!")
+
+console.log("\nDone.")
